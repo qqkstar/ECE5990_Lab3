3f286c730aaf71ce8637bb41319e9cecf051c1a0
qqkstar <xz254@cornell.edu>
2016-04-04 03:51:20 -0400

    mostly done with rolling_control.py

--- rolling_control.py
+++ rolling_control.py
@@ -16,6 +16,10 @@ left = GPIO.PWM(17, 50)
 right = GPIO.PWM(22, 50)
 left.start(0)
 right.start(0)
+left_duty = 0
+right_duty = 0
+left_info = 'idle'
+right_info = 'idle'
 
 black = 0, 0, 0
 
@@ -50,35 +54,86 @@ right_text_pos = right_text.get_rect()
 right_text_pos.centerx = 240
 right_text_pos.centery = 30
 
+quit_text = font.render("QUIT", 1, (255,250,255))
+quit_text_pos = quit_text.get_rect()
+quit_text_pos.centerx = 160
+quit_text_pos.centery = 220
+
+left_direction = font.render("", 1, (255,250,255))
+left_direction_pos = left_direction.get_rect()
+left_direction_pos.centerx = 40
+left_direction_pos.centery = 50
+
+right_direction = font.render("", 1, (255,250,255))
+right_direction_pos = right_direction.get_rect()
+right_direction_pos.centerx = 210
+right_direction_pos.centery = 50
+
+def left_direction_disp(text):
+  left_direction = font.render(text, 1, (255,250,255))
+  return left_direction
+
+def right_direction_disp(text):
+  right_direction = font.render(text, 1, (255,250,255))
+  return right_direction
+
 while 1:
   for event in pygame.event.get():
     if event.type == pygame.MOUSEBUTTONDOWN:
       p = pygame.mouse.get_pos()
       if p[0]>130 and p[0]<190 and p[1]>110 and p[1]<180:
-        #sys.exit()
         sign_toggle ^= 1
-      else:
+      elif p[0]>150 and p[0]<170 and p[1]>210 and p[1]<230:
         sys.exit()
     elif event.type == pygame.KEYDOWN:
       pressed = pygame.key.get_pressed()
       if pressed[pygame.K_1]:
         left.ChangeDutyCycle(7.61)
+        left_duty = 7.61
+        left_info = 'forward'
+        left_direction = left_direction_disp('forward')
       elif pressed[pygame.K_2]:
         left.ChangeDutyCycle(0)
+        left_duty = 0
+        left_info = 'idle'
+        left_direction = left_direction_disp('idle')
       elif pressed[pygame.K_3]:
         left.ChangeDutyCycle(6.77)
+        left_duty = 6.77
+        left_info = 'backward'
+        left_direction = left_direction_disp('backward')
       elif pressed[pygame.K_8]:
         right.ChangeDutyCycle(6.77)
+        right_duty = 6.77
+        right_info = 'forward'
+        right_direction = right_direction_disp('forward')
       elif pressed[pygame.K_9]:
         right.ChangeDutyCycle(0)
+        right_duty = 0
+        right_info = 'idle'
+        right_direction = right_direction_disp('idle')
       elif pressed[pygame.K_0]:
         right.ChangeDutyCycle(7.61)
+        right_duty = 7.61
+        right_info = 'backward'
+        right_direction = right_direction_disp('backward')
 
   screen.fill(black)
   if sign_toggle:
     screen.blit(stop, stop_rect)
+    left.ChangeDutyCycle(left_duty)
+    right.ChangeDutyCycle(right_duty)
+    left_direction = left_direction_disp(left_info)
+    right_direction = right_direction_disp(right_info)
   else:
     screen.blit(go, go_rect)
+    left.ChangeDutyCycle(0)
+    right.ChangeDutyCycle(0)
+    left_direction = left_direction_disp('stopped')
+    right_direction = left_direction_disp('stopped')
   screen.blit(left_text, left_text_pos)
   screen.blit(right_text, right_text_pos)
+  screen.blit(quit_text, quit_text_pos)
+  screen.blit(left_direction, left_direction_pos)
+  screen.blit(right_direction, right_direction_pos)
   pygame.display.flip()
