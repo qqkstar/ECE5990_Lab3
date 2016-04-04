6a9facf67fb8f09ff1f99d1d8ca1bb29d7ba2a5f
qqkstar <xz254@cornell.edu>
2016-04-04 00:08:13 -0400

    completed servo_control.py

--- servo_control.py
+++ servo_control.py
@@ -1,20 +1,31 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 
-p = GPIO.pwm(17, 50)
+p = GPIO.PWM(17, 50)
 p.start(0)
 
-duty = 7.14
+duty = 7.17
 p.ChangeDutyCycle(duty)
-print "Initial duty cycle is " + `duty`
-raw_input('Press enter to accelerate')
-for x in range(9):
-  duty -= 0.05
+print "Clockwise Orientation: initial duty cycle is " + `duty`
+time.sleep(3)
+for x in range(10):
+  duty -= 0.04
   p.ChangeDutyCycle(duty)
-  print "The current duty is " + `duty`
-  raw_input('Press enter to accelerate')
+  print "Step #" + str(x+1) + ": The current duty cycle is " + `duty`
+  time.sleep(3)
+
+duty = 7.21
+p.ChangeDutyCycle(duty)
+print "Counter-clockwise Orientation: initial duty cycle is " + `duty`
+time.sleep(3)
+for x in range(10):
+  duty += 0.04
+  p.ChangeDutyCycle(duty)
+  print "Step #" + str(x+1) + ": The current duty cycle is " + `duty`
+  time.sleep(3)
 
 p.stop()
 GPIO.cleanup()
