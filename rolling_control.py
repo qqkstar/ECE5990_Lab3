eacb4e345ba438928f30c370ff32aa901b752295
qqkstar <xz254@cornell.edu>
2016-04-04 02:47:45 -0400

    implemented key press control

--- rolling_control.py
+++ rolling_control.py
@@ -38,6 +38,18 @@ go_rect.centery = 150
 
 sign_toggle = 1
 
+font = pygame.font.Font(None, 18)
+
+left_text = font.render("LEFT MOTOR:", 1, (255,250,255))
+left_text_pos = left_text.get_rect()
+left_text_pos.centerx = 70
+left_text_pos.centery = 30
+
+right_text = font.render("RIGHT MOTOR:", 1, (255,250,255))
+right_text_pos = right_text.get_rect()
+right_text_pos.centerx = 240
+right_text_pos.centery = 30
+
 while 1:
   for event in pygame.event.get():
     if event.type == pygame.MOUSEBUTTONDOWN:
@@ -47,9 +59,26 @@ while 1:
         sign_toggle ^= 1
       else:
         sys.exit()
+    elif event.type == pygame.KEYDOWN:
+      pressed = pygame.key.get_pressed()
+      if pressed[pygame.K_1]:
+        left.ChangeDutyCycle(7.61)
+      elif pressed[pygame.K_2]:
+        left.ChangeDutyCycle(0)
+      elif pressed[pygame.K_3]:
+        left.ChangeDutyCycle(6.77)
+      elif pressed[pygame.K_8]:
+        right.ChangeDutyCycle(6.77)
+      elif pressed[pygame.K_9]:
+        right.ChangeDutyCycle(0)
+      elif pressed[pygame.K_0]:
+        right.ChangeDutyCycle(7.61)
+
   screen.fill(black)
   if sign_toggle:
     screen.blit(stop, stop_rect)
   else:
     screen.blit(go, go_rect)
+  screen.blit(left_text, left_text_pos)
+  screen.blit(right_text, right_text_pos)
   pygame.display.flip()
